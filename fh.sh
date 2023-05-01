6f63aec9615699eeeffdc41848e38ac1ceb6b078
Ruben van Staden <rvs.cipher@proton.me>
2023-05-01 09:03:05 -0600

    feat: add fzf scripts

---
+++ fh.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# fh - repeat history
+runcmd (){ perl -e 'ioctl STDOUT, 0x5412, $_ for split //, <>' ; }
+
+fh() {
+  ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed -re 's/^\s*[0-9]+\s*//' | runcmd
+}
+
+# fhe - repeat history edit
+writecmd (){ perl -e 'ioctl STDOUT, 0x5412, $_ for split //, do{ chomp($_ = <>); $_ }' ; }
+
+fhe() {
+  ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed -re 's/^\s*[0-9]+\s*//' | writecmd
+}
+
+fh
---
+++ igr.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+declare preview='bat --color=never --style=header,numbers -H {2} {1} | grep -C3 {q}'
+
+rg --color=always -n ${list_files:+-l} "$1" 2> /dev/null |
+fzf -d: \
+    --ansi \
+    --query="$1" \
+    --phony \
+    --bind="change:reload:rg -n ${list_files:+-l} --color=always {q}" \
+    --bind='enter:execute:v {1}' \
+    --preview="[[ -n {1} ]] && $preview"
