01b17ef752e652cf4dbae1fdf57a58e3faa55d34
Ruben van Staden <rvs.cipher@proton.me>
2023-05-14 22:10:17 -0600

    chore: import env var check for journal week

--- journal-day.sh
+++ journal-day.sh
@@ -4,10 +4,12 @@ set -e
 
 [[ -z "$JOURNAL" ]] && echo "ERROR: env var $JOURNAL not set" && exit 1
 
-create() {
+fn() {
 	local past=$1
 	uuid=$(date -v -"${past}d" +'%Y-%m-%d')
 	$EDITOR "${JOURNAL}/daily/${uuid}.md"
 }
 
-create "$1"
+fn "$1"
+
+exit 0
--- journal-week.sh
+++ journal-week.sh
@@ -2,15 +2,14 @@
 
 set -e
 
-create () {
+[[ -z "$JOURNAL" ]] && echo "ERROR: env var $JOURNAL not set" && exit 1
+
+fn() {
     local past=$(($1 * 7))
     uuid=$(date -v -"${past}"d +'%Y-%m-%U')
     $EDITOR "${JOURNAL}/weekly/${uuid}.md"
 }
 
-if [[ -n ${JOURNAL} ]]; then
-    create "$1"
-else
-    echo "ERROR: env var $JOURNAL not set"
-fi
+fn "$1"
 
+exit 0
